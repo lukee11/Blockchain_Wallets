1315895ea65ca8ed47e3e52eba5a81dca42f9b5b
lukee11 <ljetrade@gmail.com>
2022-05-12 16:47:41 +1000

    complete python and executable files and add images

---
+++ Instructions/Starter-Code/executable.bat
@@ -0,0 +1,5 @@
+call "C:/Users/Luke/anaconda3/Scripts/activate.bat" 
+call activate dev
+cd C:/Users/Luke/Desktop/Fintech/my_repos/Blockchain_Wallets/Instructions/Starter-Code
+call streamlit run fintech_finder.py
+pause
